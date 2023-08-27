08e950bbf5d3bf8facdfc0177ad414238b1c807b
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-08-27 11:16:40 +0200

    feedbacks

--- src/components/Feedback/Feedbacks.jsx
+++ src/components/Feedback/Feedbacks.jsx
@@ -1,16 +1,3 @@
-// import { FeedbacksBtn, Wrapper } from './Feedbacks.styled';
-// export const Feedbacks = ({ options, onLeaveFeedback }) => {
-//   return (
-//     <Wrapper>
-//       {options.map(option => (
-//         <FeedbacksBtn key={option} onClick={() => onLeaveFeedback(option)}>
-//           {option}
-//         </FeedbacksBtn>
-//       ))}
-//     </Wrapper>
-//   );
-// };
-
 import React from 'react';
 import { FeedbacksBtn, Wrapper } from './Feedbacks.styled';
 
