aad51d9ed2ef200bc06a942c9f34f95a478d4175
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-08-27 11:17:20 +0200

    statistiks

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,24 +1,3 @@
-// import { StatisticsDescr } from './Statistics.styled';
-
-// export const Statistics = ({
-//   good,
-//   neutral,
-//   bad,
-//   total,
-//   positivePercentage,
-// }) => {
-//   return (
-//     <div>
-//       <StatisticsDescr>Good: {good}</StatisticsDescr>
-//       <StatisticsDescr>Neutral: {neutral}</StatisticsDescr>
-//       <StatisticsDescr>Bad: {bad}</StatisticsDescr>
-//       <StatisticsDescr>Total: {total}</StatisticsDescr>
-//       <StatisticsDescr>
-//         Positive Percentage: {positivePercentage}%
-//       </StatisticsDescr>
-//     </div>
-//   );
-// };
 import React from 'react';
 import { StatisticsDescr } from './Statistics.styled';
 
