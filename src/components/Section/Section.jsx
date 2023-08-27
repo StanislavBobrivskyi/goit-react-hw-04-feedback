58a205fe481c37f894d02c077b1debadd53861e6
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-08-27 11:17:46 +0200

    section changes

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,14 +1,3 @@
-// import { SectionContainer, Title } from './Section.styled';
-
-// export const Section = ({ title, children }) => {
-//   return (
-//     <SectionContainer>
-//       <Title>{title}</Title>
-//       {children}
-//     </SectionContainer>
-//   );
-// };
-
 import React from 'react';
 import { SectionContainer, Title } from './Section.styled';
 
